1fdfaba7770e79f8ec668600c7f03457a9643fa3
onethejay <jhjang951@gmail.com>
2022-07-19 23:33:38 +0900

    1. HelloService - sayHello 메서드 return 수정

--- src/main/java/com/example/gitflowsample/service/HelloService.java
+++ src/main/java/com/example/gitflowsample/service/HelloService.java
@@ -5,6 +5,6 @@ import org.springframework.stereotype.Service;
 @Service
 public class HelloService {
     public String sayHello() {
-        return "Hello there";
+        return "Hello";
     }
 }
