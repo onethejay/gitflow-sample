6d6b79ceb5dbbefe68cd6c18ffddc12613fd4434
onethejay <jhjang951@gmail.com>
2022-07-19 23:29:52 +0900

    1. HelloService 생성 후 HelloController의 hello 메서드에서 사용하도록 수정

---
+++ src/main/java/com/example/gitflowsample/service/HelloService.java
@@ -0,0 +1,10 @@
+package com.example.gitflowsample.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloService {
+    public String sayHello() {
+        return "Hello there";
+    }
+}
