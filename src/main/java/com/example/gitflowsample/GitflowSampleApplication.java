82a4766945b58b8b545b191d14d43c4bbd6dea73
onethejay <jhjang951@gmail.com>
2022-07-19 22:35:08 +0900

    Initial commit

---
+++ src/main/java/com/example/gitflowsample/GitflowSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitflowsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitflowSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitflowSampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gitflowsample/GitflowSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitflowsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitflowSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
